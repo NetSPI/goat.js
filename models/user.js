7a7032835b67462eb521c6516bdab8a3c46970e2
cktricky <ken.johnson@nvisiumsecurity.com>
2014-06-10 05:42:33 -0400

    fixed validators

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ var LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: {type: DataTypes.STRING, unique: true, allowNull: false, notEmpty: true},
-	password: {type: DataTypes.STRING, allowNull: false, notEmpty: true},
+    username: {type: DataTypes.STRING, unique: true, validate: { notNull: true, notEmpty: true}},
+	password: {type: DataTypes.STRING, validate: { notNull: true, notEmpty: true}},
   },
   {
 	  dialect: 'mysql'
