06290e43e090db43279c74470ab32986e9ddd1d7
cktricky <ken.johnson@nvisiumsecurity.com>
2014-06-13 16:56:41 -0400

    just changing this around to be insecure-er

--- routes/user.js
+++ routes/user.js
@@ -18,6 +18,18 @@ exports.register = function(req, res){
 };
 
 exports.update = function(req, res) {
+	
+	if (req.body.new_password = req.body.new_password_confirmation){
+	   req.user.username = req.body.username
+	   req.user.password = req.body.new_password
+	   req.user.save()
+	}
+  
+    res.redirect('/account')
+};
+
+/*
+exports.update = function(req, res) {
 	var t = function(cb, user) {
 	   if (user) {
 		 req.user.password = req.body.new_password
@@ -29,4 +41,4 @@ exports.update = function(req, res) {
 	}
   
     res.redirect('/account')
-};
+};*/
